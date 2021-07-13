780398afa1ef8a264726f245ece1a25f0150045b
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-07-13 12:54:19 +0530

    Update InsertionSort.cpp

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,1 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class InsertionSort
+{
+
+   public:
+      void insertionSort(int arr[],int size)
+      { 
+          int i,j,cur;
+          for(i=1;i<size;i++)
+          {
+              cur=arr[i];
+              for(j=i;j>0;j--)
+              {
+                  if(cur<arr[j-1])
+                  {
+                    arr[j]=arr[j-1];
+                  }
+                  else
+                  {
+                  break;
+                  }
+              }  
+              arr[j]=cur;
+          }
+          
+         
+      }
+
+};
+
+int main()
+{
+
+ int size;
+ cout<<"Enter the size of array:";
+ cin>>size;
+ int arr[size];
+cout<<"Enter the elements in array: ";
+for(int i=0;i<size;i++)
+{
+    cin>>arr[i];
+}
+InsertionSort sort=InsertionSort();
+sort.insertionSort(arr,size);
+cout<<"The elements after sorted are: ";
+for(int i=0;i<size;i++)
+{
+    cout<<arr[i]<<" ";
+}
+cout<<endl;
+
+return 0;
+}
 
