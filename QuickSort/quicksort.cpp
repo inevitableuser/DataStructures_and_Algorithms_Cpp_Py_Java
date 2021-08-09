1db3c219ac5bed1bced03715d0debe0a7137db4a
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-09 12:34:57 +0530

    Update quicksort.cpp

--- QuickSort/quicksort.cpp
+++ QuickSort/quicksort.cpp
@@ -1,1 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void swap(vector<int> &vect, int x, int y) {
+  int temp = vect.at(x);
+  vect.at(x) = vect.at(y);
+  vect.at(y) = temp;
+}
+int partition(vector<int> &vect, int start, int end) {
+  int i = start - 1;
+  int pivot = vect.at(end);
+
+  for (int j = start; j <= end - 1; j++) {
+    if (vect.at(j) <= pivot) {
+      i++;
+      swap(vect, i, j);
+    }
+  }
+  swap(vect, i + 1, end);
+
+  return i + 1;
+}
+
+void quicksort(vector<int> &vect, int start, int end) {
+  if (start < end) {
+    int q = partition(vect, start, end);
+    quicksort(vect, start, q - 1);
+    quicksort(vect, q + 1, end);
+  }
+}
+
+int main() {
+  vector<int> vect;
+  int ele;
+  char ch = 'Y';
+
+  while (ch != 'n' && ch != 'N') {
+    cout << "Enter a ele to append: ";
+    cin >> ele;
+    vect.push_back(ele);
+    cout << "continue? y or n: ";
+    cin >> ch;
+  }
+
+  cout << "vector before sorting: ";
+  for (int i : vect)
+    cout << i << " ";
+  cout << endl;
+
+  quicksort(vect, 0, vect.size() - 1);
+
+  cout << "vector after sorting: ";
+  for (int i : vect)
+    cout << i << " ";
+  cout << endl;
+
+  return 0;
+}
 
