80ca025915ea8bf2a253c357ec0230f54e04a3fa
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-08-19 14:45:30 +0000

    Asgn: Alorithmic toolbox week1

--- AlgorithmicToolbox/maximumPairwiseProduct.cpp
+++ AlgorithmicToolbox/maximumPairwiseProduct.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main()
+{
+    int n,ele,max=0,smax;
+    cin>>n;
+    vector<int> vect;
+    for(int i=0;i<n;i++)
+    {
+        cin>>ele;
+        if(ele>=max)
+        {
+            smax=max;
+            max=ele;
+        }
+        vect.push_back(ele);
+    }
+    cout<<max*smax<<endl;
+    return 0;
+}
