8101db75e2f809caad425b7749e0448ff9639c27
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-08-20 07:14:33 +0000

    Impl: maxheap in DS (python)

--- max_heap/maxheap.py
+++ max_heap/maxheap.py
@@ -1,24 +1,75 @@
+import math
+
 class MaxHeap:
     heap=[]
     def __init__(self):
         pass
     
     def peek(self):
-        pass
+        print("The peek of the max heap is ",self.heap[0])
+
+    def swap(self,i,j):
+        heap=self.heap
+        temp=heap[i]
+        heap[i]=heap[j]
+        heap[j]=temp
+        self.heap=heap
+
+    def up_heapify(self,child):
+        if child==0:
+            return
+        parent=math.ceil(child/2)-1
+        if self.heap[child] > self.heap[parent]:
+            self.swap(child,parent)
+            child=parent
+            self.up_heapify(child)
     
     def offer(self,ele):
-        
+        self.heap.append(ele)
+        self.up_heapify(len(self.heap)-1)
+
+    def down_heapify(self,parent,size):
+
+        lchild=parent*2+1
+        rchild=parent*2+2
+        larg=parent
+        if lchild < size and self.heap[lchild] > self.heap[larg]:
+            larg=lchild
+        if rchild < size and self.heap[rchild] > self.heap[larg]:
+            larg=rchild
+        if larg!=parent:
+            self.swap(parent,larg)
+            self.down_heapify(larg,size)
+
     
     def poll(self):
-        pass
+        self.swap(0,len(self.heap)-1)
+        print("The popped ele is",self.heap[-1])
+        self.heap.pop()
+        self.down_heapify(0,len(self.heap))
+        
     
     def printheap(self):
-        pass
+        print("printing maxheap: ",end='')
+        for i in self.heap:
+            print(i,end=' ')
+        print()
+    
+    def rebuild(self):
+        size=len(self.heap)
+        l=size-1
+        p=math.ceil(l/2)-1
+        for i in range(p,-1,-1):
+            self.down_heapify(i,size)
     
     def sort(self):
-        pass
-
-
+        l=len(self.heap)-1
+        for i in range(l,0,-1):
+            self.swap(0,i)
+            self.down_heapify(0,i)
+        self.printheap()
+        print("Rebuilding sorted heap to maxheap...")
+        self.rebuild()
 
 class Main:
     def main():
@@ -28,18 +79,121 @@ class Main:
             choice=int(input("Enter your choice: "))
             if choice==1:
                 ele=int(input("Enter the ele: "))
-                mheap.offer()
+                mheap.offer(ele)
             elif choice==2:
-                mheap.poll()
+                if len(mheap.heap)!=0:
+                       mheap.poll()
+                else:
+                    print("UnderFlow maxheap is empty")
             elif choice==3:
-                mheap.peek()
+                if len(mheap.heap)!=0:
+                       mheap.peek()
+                else:
+                    print("maxheap is empty")
             elif choice==4:
-                mheap.printheap()
+                if len(mheap.heap)!=0:
+                       mheap.printheap()
+                else:
+                    print("maxheap is empty")
             elif choice==5:
-                mheap.sort()
+                if len(mheap.heap)!=0:
+                       mheap.sort()
+                else:
+                    print("maxheap is empty")
             else:
                 break
                 
         
     if __name__=="__main__":
         main()
+
+
+
+
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 1
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 2
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 3
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 4
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 5
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 6
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 7
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 8
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 9
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 1
+# Enter the ele: 10
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 3
+# The peek of the max heap is  10
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 4
+# printing maxheap: 10 9 6 7 8 2 5 1 4 3 
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 5
+# printing maxheap: 1 2 3 4 5 6 7 8 9 10 
+# Rebuilding sorted heap to maxheap...
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 4
+# printing maxheap: 10 9 7 8 5 6 3 1 4 2 
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 10
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 9
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 8
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 7
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 6
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 5
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 4
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 3
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 2
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# The popped ele is 1
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 2
+# UnderFlow maxheap is empty
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 3
+# maxheap is empty
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 4
+# maxheap is empty
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 5
+# maxheap is empty
+# 1.offer   2.Poll   3.peek   4.print   5.sort   6.exit
+# Enter your choice: 6
