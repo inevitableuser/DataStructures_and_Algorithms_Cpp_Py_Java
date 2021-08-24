078f6d071be363ce7abc36245df2422e7b69e60b
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-08-24 07:33:51 +0000

    Impl: heapsort in py

--- heapsort/heapsort.py
+++ heapsort/heapsort.py
@@ -1,1 +1,26 @@
+import heapq
 
+class Main:
+    def sort(self,Arr):
+        li=[]
+        for i in range(len(Arr)):
+            li.append(heapq.heappop(Arr))
+        return li
+
+    def main(self):
+        Arr=[int(i) for i in input("Enter your Array: ").split()]
+        heapq.heapify(Arr)
+        main=Main()
+        Arr=self.sort(Arr)
+        print("Array after heap sort:",' '.join([str(i) for i in Arr]))
+
+if __name__=="__main__":
+    Main().main()
+
+
+
+
+# OUTPUT:
+
+# Enter your Array: 7 90 45 2 48 1
+# Array after heap sort: 1 2 7 45 48 90
