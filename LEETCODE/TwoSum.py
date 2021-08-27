6fd64a40a4ff5e9c0c1761e3f37203fc4bbec2c5
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-08-27 07:34:30 +0000

    LEET: problem 1 in py

--- LEETCODE/TwoSum.py
+++ LEETCODE/TwoSum.py
@@ -1,1 +1,53 @@
+# runs with 88% better tc and 20% better sc
 
+class Solution:
+    def twoSum(self, nums, target):
+        visited={}
+        for i in range(len(nums)):
+            remain=target-nums[i]
+            if remain in visited.keys():
+                return [visited[remain],i]
+            visited[nums[i]]=i
+            
+
+
+sol=Solution()
+print(sol.twoSum([3,2,4],6))
+
+
+
+
+# runs with 30% better tc and   80 % better sc
+
+
+# class Solution:
+#     def twoSum(self, nums, target):
+#         ans=[]
+#         count=0
+#         for i,x in enumerate(nums):
+#             if target - x in nums[:i]+nums[i+1:]:
+#                 target =target-x
+#                 ans.append(i)
+#                 p=i+1
+#                 ans.append(nums[p:].index(target)+p)
+#                 break
+#         return ans
+                
+            
+
+
+# sol=Solution()
+# print(sol.twoSum([2,5,5,11],10))
+
+
+
+
+
+
+
+
+# [-3,4,3,90]
+# 0
+
+
+# [0,1]
