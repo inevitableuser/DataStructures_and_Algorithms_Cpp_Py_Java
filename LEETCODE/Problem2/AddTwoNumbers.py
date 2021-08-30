74595f9b999f74cb833b9121d2b8addd11347b10
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-30 17:19:13 +0530

    Leet: problem2 in py

--- LEETCODE/Problem2/AddTwoNumbers.py
+++ LEETCODE/Problem2/AddTwoNumbers.py
@@ -70,3 +70,54 @@ class Main:
 
 if __name__=="__main__":
     Main().main()
+
+
+
+
+
+
+# space complexity O(1) approach
+
+# def addTwoNumbers(self, l1, l2):          
+    #     x,y=l1,l2
+    #     flag=0
+    #     while x!=None and y!=None:
+    #         x=x.next
+    #         y=y.next
+
+    #     if y!=None:
+    #         flag=1
+    #         p,q=l2,l1
+    #     else:
+    #         p,q=l1,l2
+    #     carry=0
+    #     while p!=None and q!=None:
+    #         p.val+=q.val
+    #         if len(str(p.val))!=1:
+    #             carry=p.val//10
+    #             p.val=p.val%10
+    #             if p.next ==None:
+    #                 p.next=ListNode(carry)
+    #             else:
+    #                p.next.val+=carry
+    #         p=p.next
+    #         q=q.next
+ 
+    #     if p!=None:
+    #         if len(str(p.val))!=1:
+    #             while p!=None:
+    #                 carry=p.val//10
+    #                 p.val%=10
+    #                 if p.next!=None:
+    #                     p.next.val+=carry
+    #                 if p.next==None and carry!=0:
+    #                     p.next=ListNode(carry)
+    #                 p=p.next
+
+    #     if flag==1:
+    #         return l2
+    #     else:
+    #         return l1
+     
+     
+     
