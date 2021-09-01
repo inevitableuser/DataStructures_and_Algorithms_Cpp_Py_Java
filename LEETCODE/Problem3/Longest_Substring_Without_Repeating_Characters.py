3d1205f03ef96793bf653e28d7064e38bfdeb802
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-09-01 18:57:51 +0530

    Update Longest_Substring_Without_Repeating_Characters.py

--- LEETCODE/Problem3/Longest_Substring_Without_Repeating_Characters.py
+++ LEETCODE/Problem3/Longest_Substring_Without_Repeating_Characters.py
@@ -1,1 +1,14 @@
+#time limit exceeding (bruteforce approach)
 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        li=[]
+        maxx=0
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                x=s[i:j+1]
+                l=len(set(x))
+                ol=len(x)
+                if l > maxx and ol==l :
+                     maxx=l
+        return maxx
