10afa10af21c2c13687f515244807800637544f3
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-09-02 10:51:39 +0530

    Update Longest_Substring_Without_Repeating_Characters.py

--- LEETCODE/Problem3/Longest_Substring_Without_Repeating_Characters.py
+++ LEETCODE/Problem3/Longest_Substring_Without_Repeating_Characters.py
@@ -1,14 +1,30 @@
-#time limit exceeding (bruteforce approach)
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        li=[]
-        maxx=0
-        for i in range(len(s)):
-            for j in range(i,len(s)):
-                x=s[i:j+1]
-                l=len(set(x))
-                ol=len(x)
-                if l > maxx and ol==l :
-                     maxx=l
-        return maxx
+    def lengthOfLongestSubstring(self, s):
+        max_string=''
+        maxcount=0
+        p=0
+        while p!=len(s):
+            i=s[p]
+            if i in max_string:
+                l=len(max_string)
+                if l > maxcount:
+                    maxcount=l
+                max_string=''
+                p=s.index(i)+1
+                s=s[p:]
+                p=0
+            else:
+                max_string+=i
+                p+=1
+        l=len(max_string)
+        if l > maxcount:
+            maxcount=l
+        return maxcount
+                
+            
+class Main:
+    def main(self):
+       print(Solution().lengthOfLongestSubstring(input()))
+if __name__=="__main__":
+    Main().main()
