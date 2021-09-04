b13908eaab73cf4dc2fa9ec3e54f0ecc8f6865ce
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-09-04 07:30:59 +0000

    Leet: problem3 in cpp

--- LEETCODE/Problem3/LongestSubstringUnique.cpp
+++ LEETCODE/Problem3/LongestSubstringUnique.cpp
@@ -1,1 +1,42 @@
+// Runtime: 8 ms, faster than 84.95% of C++ online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 8.2 MB, less than 72.16% of C++ online submissions for Longest Substring Without Repeating Characters.
 
+# include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        map<char,int> mp;
+        int max=0,start=0,p;
+        for(int i=0;i<s.length();i++)
+        {
+             if(mp.count(s[i]))
+             {
+                 p=mp.at(s[i])+1;
+                 if(p>start)
+                 {
+                     start=p;
+                 }
+             }
+             p=i-start+1;
+             if(p>max)
+             {
+                 max=p;
+             }
+
+             mp[s[i]]=i;
+        }
+        return max;
+    }
+};
+
+int main()
+{
+    string st="abcabcbb";
+
+    Solution sol=Solution();
+    cout<<sol.lengthOfLongestSubstring(st)<<endl;
+    return 0;
+}
